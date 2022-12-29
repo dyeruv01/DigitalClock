d57473e7126843ca4e3657d3bade9a8f1a3115fc
Ruwanthi Weerakoon <120965375+dyeruv01@users.noreply.github.com>
2022-12-29 22:48:51 +0530

    commit all

---
+++ index.js
@@ -0,0 +1,36 @@
+const hourE1 = document.getElementById("hour")
+const minuteE1 = document.getElementById("minutes")
+const secendsE1 = document.getElementById("seconds")
+const ampmE1 = document.getElementById("ampm")
+
+
+function updateClock()
+{
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let ampm = "AM"
+
+    if(h > 12)
+    {
+        h = h-12
+        ampm="PM"
+    }
+
+    h= h<10?"0"+h : h;
+    m= m<10?"0"+h : m;
+    s= s<10?"0"+h : s;
+
+
+
+    hourE1.innerText=h;
+    minuteE1.innerText=m;
+    secendsE1.innerText=s;
+    ampmE1, (innerText=ampm);
+    setTimeout(()=>
+    {
+        updateClock()
+    },1000)
+}
+
+updateClock()
